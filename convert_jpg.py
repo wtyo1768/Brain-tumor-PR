b7cbb532f0db9610e2289305ed995682a0bbd919
Yang <egg20001225@gmail.com>
2021-11-07 16:12:37 +0800

    merge nonPR and PR block

--- convert_jpg.py
+++ convert_jpg.py
@@ -1,3 +1,4 @@
+from typing import Counter
 import pandas as pd
 from matplotlib import pyplot as plt
 import pydicom
@@ -54,91 +55,81 @@ print("******************************************************")
 folder_path = f"{dcm_folder_path}/non PR"
 jpg_folder_path = f"{dcm_folder_path}/non_PR_jpg"
 
+jpg_folder_path2 = f"{dcm_folder_path}/PR_jpg"
+dcm_folder_path2 = f"{dcm_folder_path}/PR"
+
 error_patient = []
 MRI_TYPE = ['T1', 'T1c', 'Flair', 'T2']
 for d in MRI_TYPE:
-    os.makedirs(f'{jpg_folder_path}/{d}')
+    os.makedirs(f'{jpg_folder_path}/{d}')  # for non PR
+    os.makedirs(f'{jpg_folder_path2}/{d}')  # for PR
 
 print("convert start : wait......")
-# ***************************non_PR block********************************************************
-for patient_id in os.listdir(folder_path):
-    if patient_id in ['16962871', '14185785']:  # data loss
-        continue
-    patient_path = os.path.join(folder_path, patient_id)
-    picture_count = set()
-
-    for n, image in enumerate(os.listdir(patient_path)):
-        ds = pydicom.dcmread(os.path.join(patient_path, image))
-        # non _dict (every image should be judged 4 times)
+# ******************************convert block***********************************************
+dir_count = 0
+path_list = os.listdir(folder_path)
+while(True):
+    for patient_id in path_list:
+        if patient_id in ['16962871', '14185785']:  # data loss
+            continue
+        print(type(os.listdir(folder_path)))
+        print(patient_id)
+        picture_count = set()
         T1c = img_dict[int(patient_id)][0]
         T1 = img_dict[int(patient_id)][1]
         T2 = img_dict[int(patient_id)][2]
         Flair = img_dict[int(patient_id)][3]
 
-        if ds.SeriesNumber == T1[0] and ds.InstanceNumber == T1[1]:
-            convert(ds, f'{jpg_folder_path}/T1', patient_id)
-            picture_count.add('T1')
-        if ds.SeriesNumber == T1c[0] and ds.InstanceNumber == T1c[1]:
-            convert(ds, f'{jpg_folder_path}/T1c', patient_id)
-            picture_count.add('T1c')
-        if ds.SeriesNumber == T2[0] and ds.InstanceNumber == T2[1]:
-            convert(ds, f'{jpg_folder_path}/T2', patient_id)
-            picture_count.add('T2')
-        if ds.SeriesNumber == Flair[0] and ds.InstanceNumber == Flair[1]:
-            convert(ds, f'{jpg_folder_path}/Flair', patient_id)
-            picture_count.add('Flair')
+        if dir_count == 0:  # non_PR
+            patient_path = os.path.join(folder_path, patient_id)
+            for n, image in enumerate(os.listdir(patient_path)):
+                ds = pydicom.dcmread(os.path.join(patient_path, image))
+                # non _dict (every image should be judged 4 times)
 
-    if len(picture_count) != 4:
-        error_patient.append(patient_id)
-    # if patient_id=='16962871':
-    #     print(picture_count)
-# *******************************************************************************************************
-if error_patient != []:
-    print(error_patient)
+                if ds.SeriesNumber == T1[0] and ds.InstanceNumber == T1[1]:
+                    convert(ds, f'{jpg_folder_path}/T1', patient_id)
+                    picture_count.add('T1')
+                if ds.SeriesNumber == T1c[0] and ds.InstanceNumber == T1c[1]:
+                    convert(ds, f'{jpg_folder_path}/T1c', patient_id)
+                    picture_count.add('T1c')
+                if ds.SeriesNumber == T2[0] and ds.InstanceNumber == T2[1]:
+                    convert(ds, f'{jpg_folder_path}/T2', patient_id)
+                    picture_count.add('T2')
+                if ds.SeriesNumber == Flair[0] and ds.InstanceNumber == Flair[1]:
+                    convert(ds, f'{jpg_folder_path}/Flair', patient_id)
+                    picture_count.add('Flair')
 
+        elif dir_count == 1:  # PR
+            patient_path = os.path.join(dcm_folder_path2, patient_id)
+            for n, image in enumerate(os.listdir(patient_path)):
+                ds = pydicom.dcmread(os.path.join(
+                    patient_path, image), force=True)
+                try:
+                    if ds.SeriesNumber == T1[0] and ds.InstanceNumber == T1[1]:
+                        convert(ds, f'{jpg_folder_path2}/T1', patient_id)
+                        picture_count.add('T1')
+                    if ds.SeriesNumber == T1c[0] and ds.InstanceNumber == T1c[1]:
+                        convert(ds, f'{jpg_folder_path2}/T1c', patient_id)
+                        picture_count.add('T1c')
+                    if ds.SeriesNumber == T2[0] and ds.InstanceNumber == T2[1]:
+                        convert(ds, f'{jpg_folder_path2}/T2', patient_id)
+                        picture_count.add('T2')
+                    if ds.SeriesNumber == Flair[0] and ds.InstanceNumber == Flair[1]:
+                        convert(ds, f'{jpg_folder_path2}/Flair', patient_id)
+                        picture_count.add('Flair')
+                except:
+                    print(os.error)
+                    error_patient.append(patient_id)
 
-# ***************************************PR block******************************************************
-jpg_folder_path = f"{dcm_folder_path}/PR_jpg"
-dcm_folder_path = f"{dcm_folder_path}/PR"
-
-error_patient = []
-#MRI_TYPE = ['T1', 'T1c', 'Flair', 'T2']
-for d in MRI_TYPE:
-    os.makedirs(f'{jpg_folder_path}/{d}')
-
-for patient_id in os.listdir(dcm_folder_path):
-    patient_path = os.path.join(dcm_folder_path, patient_id)
-    picture_count = set()
-
-    for n, image in enumerate(os.listdir(patient_path)):
-        ds = pydicom.dcmread(os.path.join(patient_path, image), force=True)
-
-        T1c = img_dict[int(patient_id)][0]
-        T1 = img_dict[int(patient_id)][1]
-        T2 = img_dict[int(patient_id)][2]
-        Flair = img_dict[int(patient_id)][3]
-        try:
-            if ds.SeriesNumber == T1[0] and ds.InstanceNumber == T1[1]:
-                convert(ds, f'{jpg_folder_path}/T1', patient_id)
-                picture_count.add('T1')
-            if ds.SeriesNumber == T1c[0] and ds.InstanceNumber == T1c[1]:
-                convert(ds, f'{jpg_folder_path}/T1c', patient_id)
-                picture_count.add('T1c')
-            if ds.SeriesNumber == T2[0] and ds.InstanceNumber == T2[1]:
-                convert(ds, f'{jpg_folder_path}/T2', patient_id)
-                picture_count.add('T2')
-            if ds.SeriesNumber == Flair[0] and ds.InstanceNumber == Flair[1]:
-                convert(ds, f'{jpg_folder_path}/Flair', patient_id)
-                picture_count.add('Flair')
-        except:
-            print(os.error)
+        if len(picture_count) != 4:
             error_patient.append(patient_id)
 
-    if len(picture_count) != 4:
-        error_patient.append(patient_id)
+    dir_count += 1
+    if dir_count >= 2:
+        break
+    path_list[:] = os.listdir(dcm_folder_path2)
 # *******************************************************************************************************
 if error_patient != [] and error_patient != ['10027124']:
     print("error patient", error_patient)
 print("convert successfully!")
-
 # another data error found -> patient 10027124 (dicomfile loss SeriesNumber data)
